8e49a9d5488f7cf12a21046799695bcb1de13e21
Robert Pringle <46602946+pringyy@users.noreply.github.com>
2022-02-03 13:12:03 +0000

    comments added

--- src/main.py
+++ src/main.py
@@ -9,18 +9,28 @@ def function1(X: list, m: Decimal, c: Decimal) -> list:
 
     '''
     try:
+        #Checks if data types of arguments passed in are correct
         if type(X) != list: raise TypeError("Argument passed into parameter 'channel' is not of type list.")
         if type(m) != Decimal: raise TypeError("Argument passed into parameter 'm' is not of type Decimal.")
-        if type(c) != Decimal: raise TypeError("Argument passed into parameter '' is not of type Decimal.")
+        if type(c) != Decimal: raise TypeError("Argument passed into parameter 'c' is not of type Decimal.")
         return [m * i + c for i in X]
     except Exception as e:
         print("Error in function1(): " + str(e))
+        sys.exit(1)
        
 def function2(channel1: list, channel2: list) -> (list, Decimal):
-    ''' This function takes the values of channels (a list of floating point numbers) and adds
-    them to each other and returns a combined list
+    ''' This function takes two channel lists of equal lengths and calculates a new list 
+    where each element is the sum of the corresponding elements in the original lists.
+    It also calculates the average number in this new list. The function then returns the new list
+    and the average value.
 
-    
+    Parameters:
+    channel1 (list): a list of floating point numbers stored as Decimal type,
+    channel2 (list): a list of floating point numbers stored as Decimal type
+
+    Returns:
+    list: a list of the sum of the corresponding elements in channel1 and channel2,
+    Decimal: the mean of the values in the return list
     '''
     try:
         if type(channel1) != list: raise TypeError("Argument passed into parameter 'list1' is not of type list.")
@@ -29,24 +39,39 @@ def function2(channel1: list, channel2: list) -> (list, Decimal):
         return outputList, mean(outputList)
     except Exception as e:
         print("Error in function2(): " + str(e))
+        sys.exit(1)
    
 def function3(channel: list) -> list:
     '''
-    This function takes a channel (a list of floating point numbers) and returns a list of the same length
-    but every value has been replaced with 1/channel value
+    This function takes a channel (a list of floating point numbers) and returns a new list of the same length
+    but every value is 1 divided by each value in the list passed into the function. This function handles the
+    division by zero error as if it identifies a 0 to be in the original list it will just set the value to 0
+    in the return list instead of throwing an error.
 
+    Parameters:
+    channel (list): a list of floating point numbers
+
+    Returns:
+    list: a list where 1 has been divided by every value in channel
     '''
     try:
         if type(channel) != list: raise TypeError("Argument passed into parameter 'channel' is not of type list.")
         return [0 if i == 0 else 1/i for i in channel]
     except Exception as e:
         print("Error in function3(): " + str(e))
+        sys.exit(1)
 
 def function4(channel: list, metric: Decimal) -> list:
-    ''' This function takes a channel (list of floating point numbers) and adds a metric 
-    (a single floating point number) on to each value present in the channel list.
+    ''' This function takes a channel list and adds a metric on to each 
+    value present in the original list and then returns the result of 
+    this as a new list.
 
-    
+    Parameters:
+    channel (list): a list of floating point numbers stored as Decimal type
+    metric (Decimal): a single floating point number
+
+    Returns:
+    list: a list where each value in channel has had the metric parameter added 
     '''
     try:
         if type(channel) != list: raise TypeError("Argument passed into parameter 'channel' is not of type list.")
@@ -54,10 +79,20 @@ def function4(channel: list, metric: Decimal) -> list:
         return [i + metric for i in channel]
     except Exception as e:
         print("Error in function4(): " + str(e))
+        sys.exit(1)
 
 def readChannel(dir: str) -> list:
     '''
+    This function takes a filename (inputted by the user) and then read in the values for a single 
+    channel (a sequence of floating point numbers) from a .txt file in src/input and stores them
+    in an array. Each value is set to a Decimal variable type, so there is no floating-point errors 
+    when carrying out calculations with the data. 
 
+    Parameters:
+    dir (str): the filename which the user wants to read the channel data from
+
+    Returns:
+    list: a channel list of floating point numbers where each number is stored as a Decimal variable type
     '''
     try:
         print("Reading input for channel...")
@@ -79,7 +114,17 @@ def readChannel(dir: str) -> list:
 
 def readParameters(dir: str) -> (Decimal, Decimal):
     '''
+    This function takes a filename (inputted by the user) and then read in the values for two 
+    parameters (single floating point numbers) in a .txt file stored in src/input. It then takes
+    this floating point number and stores it as a Decimal variable type so there is no 
+    floating-point errors when carrying out calculations.
 
+    Parameters:
+    dir (str): the filename which the user wants to read the parameter data from
+
+    Returns:
+    Decimal: a single floating point parameter value stored as a Decimal variable type
+    Decimal: a single floating point parameter value stored as a Decimal variable type
     '''
     try:
         print("Reading input for paramaters m and c...")
@@ -100,23 +145,38 @@ def readParameters(dir: str) -> (Decimal, Decimal):
         return readParameters(input("Enter the name of the file which stores the parameters in src/inputs: ") or "parameters.txt")
 
 def createOutputDir() -> str:
-    '''
+    '''This creates a unique folder for the output to be written to everytime the
+    program is run. It makes it unique by adding the current date and time to the 
+    end of the folder name. 
+    
+    Parameters:
+    None
 
+    Returns
+    dir (str): returns the string of the created output folder
     '''
     dir = "output/output "+time.strftime("%d-%m-%Y %H%M%S")+"/"
     if not os.path.exists(dir): os.makedirs(dir)
     return dir
     
 def writeOutput(dict, dir, fileName) -> None:
-    '''
+    '''This function outputs the keys and values stored in a dictionary as a 
+    string in a .txt file. This file is exported to a folder in src/outputs.
 
+    Parameters:
+    dict (dict): the dictionary which keys and values are to be outputted,
+    dir (str): the folder name to be written to (generated in createOutputDir()),
+    fileName (str): the name of the file being exported
+
+    Returns:
+    None
     '''
     print("Exporting " + fileName + " data...")
     try:
         with open(dir+fileName, "w") as f:        
             for key in dict:
                 if isinstance(dict[key], list):
-                    f.write(key + ", " + floatArrayToString(dict[key]) + "\n")
+                    f.write(key + ", " + listToString(dict[key]) + "\n")
                 else:
                     f.write(key + ", " + str(dict[key]) + "\n")
         print(fileName + " successfully exported to src/"+dir)
@@ -124,13 +184,24 @@ def writeOutput(dict, dir, fileName) -> None:
         print("Error while exporting " + fileName + ": " + str(e))
         sys.exit(1)
 
-def floatArrayToString(lst:[float]) -> str:
-    return ', '.join([str(float(n)) for n in lst])
+def listToString(lst:list) -> str:
+    '''This function takes a list and converts it to a string.
 
-    
+    Parameters:
+    lst (list): a list that is wanted to be converted to a string
+
+    Returns:
+    str: a stringfied version of lst
+    '''
+    try:
+        if type(lst) != list: raise TypeError("Argument passed into parameter 'lst' is not of type list.")
+        return ', '.join([str(float(i)) if type(i) == Decimal else str(i) for i in lst])
+    except Exception as e:
+        print("Error in listToString(): " + str(e))
+        sys.exit(1)
+
 def main():  
 
-    print(function4([Decimal('1'),Decimal('0.25'),Decimal('1.25'),Decimal('10.0')], Decimal('2.4')))
     channels = dict.fromkeys(['X', 'Y', 'A', 'B', 'C']) #Initialise dicitonary keys with no values to store channel arrays
     parameters = dict.fromkeys(['m', 'c']) #Initialise dictionary keys with no values to store parameters 
     metrics = dict.fromkeys(['b']) #Initialise dictionary keys with no values to store metrics 
@@ -144,7 +215,6 @@ def main():
     channels['B'], metrics['b'] = function2(channels['A'],channels['Y'])
     channels['C'] = function4(channels['X'], metrics['b'])
     print("Data processed succesfully. The value of b for the provided data and parameters is " + str(metrics['b']))
-
 
     dir = createOutputDir()
     writeOutput(metrics, dir, "metric_data.txt")
--- src/test.py
+++ src/test.py
@@ -1,9 +1,7 @@
 import unittest
-from main import function1, function2, function3, function4, floatArrayToString
+from main import function1, function2, function3, function4, listToString
 from decimal import *
 import sys
-
-
 
 class TestFunctions(unittest.TestCase):
 
@@ -14,80 +12,91 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual(function1([Decimal('5'), Decimal('3'), Decimal('1'), Decimal('10')], Decimal('5'), Decimal('2')), [Decimal('27'), Decimal('17'), Decimal('7'), Decimal('52')], "Should be [Decimal('27'), Decimal('17'), Decimal('7'), Decimal('52')]")
         self.assertEqual(function1([Decimal('0.5'), Decimal('1.2'), Decimal('2.5')], Decimal('2'), Decimal('1')), [Decimal('2'), Decimal('3.4'), Decimal('6')], "[Decimal('2'), Decimal('3.4'), Decimal('6')]")
     
-    def test_function1_firstArgumentIsNotListType_shouldThrowError(self):
-        self.assertRaises(TypeError, function1("Decimal('0.5'), Decimal('1.2'), Decimal('2.5')", Decimal('2'), Decimal('1')))
-        self.assertRaises(TypeError, function1(5, Decimal('2'), Decimal('1')))
-        self.assertRaises(TypeError, function1(True, Decimal('2'), Decimal('1')))
-        self.assertRaises(TypeError, function1(1.21321312, Decimal('2'), Decimal('1')))
+    def test_function1_firstArgumentIsNotListType_shouldThrowErrorAndStopSystem(self):
+        with self.assertRaises((TypeError, SystemExit)): function1("Decimal('0.5'), Decimal('1.2'), Decimal('2.5')", Decimal('2'), Decimal('1'))
+        with self.assertRaises((TypeError, SystemExit)): function1(5, Decimal('2'), Decimal('1'))
+        with self.assertRaises((TypeError, SystemExit)): function1(True, Decimal('2'), Decimal('1'))
+        with self.assertRaises((TypeError, SystemExit)): function1(1.21321312, Decimal('2'), Decimal('1'))
 
-    def test_function1_secondArgumentIsNotDecimalType_shouldThrowError(self):
-        self.assertRaises(TypeError, function1([Decimal('0.524234234'), Decimal('1.223423423'), Decimal('2.523423423')], 2.512321321, Decimal('1')))
-        self.assertRaises(TypeError, function1([Decimal('0.12312412421'), Decimal('11.212312312'), Decimal('2.51312')], "Hello", Decimal('1')))
-        self.assertRaises(TypeError, function1([Decimal('0.523432'), Decimal('1.23434'), Decimal('2.5')], True, Decimal('1')))
-        self.assertRaises(TypeError, function1([Decimal('0.5'), Decimal('1.2'), Decimal('2.5')], 54, Decimal('1')))
+    def test_function1_secondArgumentIsNotDecimalType_shouldThrowErrorAndStopSystem(self):  
+        with self.assertRaises((TypeError, SystemExit)): function1([Decimal('0.524234234'), Decimal('1.223423423'), Decimal('2.523423423')], 2.512321321, Decimal('1'))
+        with self.assertRaises((TypeError, SystemExit)): function1([Decimal('0.12312412421'), Decimal('11.212312312'), Decimal('2.51312')], "Hello", Decimal('1'))
+        with self.assertRaises((TypeError, SystemExit)): function1([Decimal('0.523432'), Decimal('1.23434'), Decimal('2.5')], True, Decimal('1'))
+        with self.assertRaises((TypeError, SystemExit)): function1([Decimal('0.5'), Decimal('1.2'), Decimal('2.5')], 54, Decimal('1'))
 
-    def test_function1_thirdArgumentIsNotDecimalType_shouldThrowError(self):
-        self.assertRaises(TypeError, function1([Decimal('0.5'), Decimal('1.2'), Decimal('2.5')], Decimal('1'), 2.512321321))
-        self.assertRaises(TypeError, function1([Decimal('0.5'), Decimal('1.2'), Decimal('2.5')], Decimal('1'), "Hello"))
-        self.assertRaises(TypeError, function1([Decimal('0.5'), Decimal('1.2'), Decimal('2.5')], Decimal('1'), True))
-        self.assertRaises(TypeError, function1([Decimal('0.5'), Decimal('1.2'), Decimal('2.5')], Decimal('1'), 54))
-
+    def test_function1_thirdArgumentIsNotDecimalType_shouldThrowErrorAndStopSystem(self):
+        with self.assertRaises((TypeError, SystemExit)): function1([Decimal('0.5'), Decimal('1.2'), Decimal('2.5')], Decimal('1'), 2.512321321)
+        with self.assertRaises((TypeError, SystemExit)): function1([Decimal('0.5'), Decimal('1.2'), Decimal('2.5')], Decimal('1'), "Hello")
+        with self.assertRaises((TypeError, SystemExit)): function1([Decimal('0.5'), Decimal('1.2'), Decimal('2.5')], Decimal('1'), True)
+        with self.assertRaises((TypeError, SystemExit)): function1([Decimal('0.5'), Decimal('1.2'), Decimal('2.5')], Decimal('1'), 54)
 
     #Tests for function2() in main.py:
     def test_function2_allArgumentsAreValidInputs_shouldReturnExpectedValues(self):
-        output1, output2 = function2([Decimal('1'),Decimal('2')], [Decimal('3'),Decimal('4')])
-        self.assertEqual(output1, [Decimal('4'),Decimal('6')])
-        self.assertEqual(output2, Decimal('5'))
+        output1, output2 = function2([Decimal('1'),Decimal('2')], [Decimal('3'),Decimal('4')],)
+        self.assertEqual(output1, [Decimal('4'),Decimal('6')], "Should be [Decimal('4'),Decimal('6')]")
+        self.assertEqual(output2, Decimal('5'), "Should be Decimal('5')")
         output1, output2 = function2([Decimal('1.382932'),Decimal('2.12321312'),Decimal('0.12321312'),Decimal('0.1')], [Decimal('3.12312'),Decimal('4.21312'),Decimal('6.312'),Decimal('0.56')])
-        self.assertEqual(output1, [Decimal('4.506052'),Decimal('6.33633312'), Decimal('6.43521312'),Decimal('0.66')])
-        self.assertEqual(output2, Decimal('4.48439956'))
+        self.assertEqual(output1, [Decimal('4.506052'),Decimal('6.33633312'), Decimal('6.43521312'),Decimal('0.66')], "Should be [Decimal('4.506052'),Decimal('6.33633312'), Decimal('6.43521312'),Decimal('0.66')]")
+        self.assertEqual(output2, Decimal('4.48439956'), "Should be Decimal('4.48439956')")
     
-    def test_function2_firstArgumentIsNotListType_shouldThrowError(self):
-        self.assertRaises(TypeError, function2(None, [1, 2, 3, 4, 5]))
-        self.assertRaises(TypeError, function2("Hello",[Decimal('1'),Decimal('2')]))
-        self.assertRaises(TypeError, function2(123456, [Decimal('1.382932'),Decimal('2.12321312'),Decimal('0.12321312'),Decimal('0.1')]))
-        self.assertRaises(TypeError, function2(Decimal('5.2123213'), [Decimal('4'),Decimal('6')]))
+    def test_function2_firstArgumentIsNotListType_shouldThrowErrorAndStopSystem(self):
+        with self.assertRaises((TypeError, SystemExit)): function2(None, [1, 2, 3, 4, 5])
+        with self.assertRaises((TypeError, SystemExit)): function2("Hello",[Decimal('1'),Decimal('2')])
+        with self.assertRaises((TypeError, SystemExit)): function2(123456, [Decimal('1.382932'),Decimal('2.12321312'),Decimal('0.12321312'),Decimal('0.1')])
+        with self.assertRaises((TypeError, SystemExit)): function2(Decimal('5.2123213'), [Decimal('4'),Decimal('6')])
     
-    def test_function2_secondArgumentIsNotListType_shouldThrowError(self):
-        self.assertRaises(TypeError, function2([1, 2, 3, 4, 5], None))
-        self.assertRaises(TypeError, function2([Decimal('1'),Decimal('2')], "Hello"))
-        self.assertRaises(TypeError, function2([Decimal('1.382932'),Decimal('2.12321312'),Decimal('0.12321312'),Decimal('0.1')], 123456))
-        self.assertRaises(TypeError, function2([Decimal('4'),Decimal('6')], Decimal('5.2123213')))
+    def test_function2_secondArgumentIsNotListType_shouldThrowErrorAndStopSystem(self):
+        with self.assertRaises((TypeError, SystemExit)): function2([1, 2, 3, 4, 5], None)
+        with self.assertRaises((TypeError, SystemExit)): function2([Decimal('1'),Decimal('2')], "Hello")
+        with self.assertRaises((TypeError, SystemExit)): function2([Decimal('1.382932'),Decimal('2.12321312'),Decimal('0.12321312'),Decimal('0.1')], 123456)
+        with self.assertRaises((TypeError, SystemExit)): function2([Decimal('4'),Decimal('6')], Decimal('5.2123213'))
     
     #Tests for function3() in main.py:
     def test_function3_allArgumentsAreValidInputs_shouldReturnExpectedValues(self):
-        self.assertEqual(function3([0,2,4,10]), [0, 0.5, 0.25, 0.1], "Should be [0, 0.5, 0.25, 0.1]")
+        self.assertEqual(function3([2,2,4,10]), [0.5, 0.5, 0.25, 0.1], "Should be [0.5, 0.5, 0.25, 0.1]")
+        self.assertEqual(function3([Decimal('1.0'),Decimal('0.25'),Decimal('1.25'),Decimal('10.0')]), [Decimal('1.0'), Decimal('4'), Decimal('0.8'), Decimal('0.1')], "Should be [Decimal('1.0'), Decimal('4'), Decimal('0.8'), Decimal('0.1')]")
+        self.assertEqual(function3([Decimal('0.25'),Decimal('1.25'),Decimal('10.0'), Decimal('0')]), [Decimal('4'), Decimal('0.8'), Decimal('0.1'), Decimal('0')], "Should be [Decimal('4'), Decimal('0.8'), Decimal('0.1'), Decimal('0')]")
        
-    def test_function3_onlyArgumentIsNotListType_shouldThrowError(self):
-        self.assertRaises(TypeError, function3("hello"))
-        self.assertRaises(TypeError, function3({'a':'b', 'c':'d'}))
-        self.assertRaises(TypeError, function3(Decimal('1.212131213213')))
-        self.assertRaises(TypeError, function3(1021312312321))
+    def test_function3_onlyArgumentIsNotListType_shouldThrowErrorAndStopSystem(self):
+        with self.assertRaises((TypeError, SystemExit)): function3("hello")
+        with self.assertRaises((TypeError, SystemExit)): function3({'a':'b', 'c':'d'})
+        with self.assertRaises((TypeError, SystemExit)): function3(Decimal('1.212131213213'))
+        with self.assertRaises((TypeError, SystemExit)): function3(1021312312321)
 
     def test_function3_inputListContainsNumberZero_shouldHandleDivisionByZeroError(self):
         self.assertEqual(function3([Decimal('0'),Decimal('0.25'),Decimal('1.25'),Decimal('10.0')]), [Decimal('0'), Decimal('4'), Decimal('0.8'), Decimal('0.1')], "Should be [Decimal(0), Decimal(4), Decimal(0.8), Decimal(0.1)]")
-        self.assertEqual(function3([10, 0, 25, 20]), [0.1, 0, 0.04, 0.05], "Should be [0.1, 0, 0.04, 0.05]") 
-
+        self.assertEqual(function3([10, 0, 25, 20]), [0.1, 0, 0.04, 0.05], "Should be [0.1, 0, 0.04, 0.05]")
+        self.assertEqual(function3([Decimal('1'), Decimal('50'), Decimal('0'), Decimal('20')]), [Decimal('1'), Decimal('0.02'), Decimal('0'),Decimal('0.05')], "Should be [Decimal('1'), Decimal('0.02'), Decimal('0'),Decimal('0.05')]")
+      
      #Tests for function4() in main.py:
     def test_function4_allArgumentsAreValidInputs_shouldReturnExpectedValues(self):
-        self.assertEqual(function4([Decimal('1'),Decimal('0.25'),Decimal('1.25'),Decimal('10.0')], Decimal('2.4')), [Decimal('3.4'), Decimal('2.65'), Decimal('3.65'), Decimal('12.4')], "Should be [Decimal('2.4'), Decimal('2.65'), Decimal('3.65'), Decimal('12.4')")
+        self.assertEqual(function4([Decimal('1'),Decimal('0.25'),Decimal('1.25'),Decimal('10.0')], Decimal('2.4')), [Decimal('3.4'), Decimal('2.65'), Decimal('3.65'), Decimal('12.4')], "Should be [Decimal('2.4'), Decimal('2.65'), Decimal('3.65'), Decimal('12.4')]")
+        self.assertEqual(function4([Decimal('1.0005'),Decimal('2.0005'),Decimal('5.2532'),Decimal('0.0234')], Decimal('1.1')), [Decimal('2.1005'), Decimal('3.1005'), Decimal('6.3532'), Decimal('1.1234')], "Should be [Decimal('2.1005'), Decimal('3.1005'), Decimal('6.3532'), Decimal('1.1234')]")
+        self.assertEqual(function4([Decimal('1'),Decimal('2'), Decimal('3'), Decimal('4'),Decimal('5')], Decimal('1')), [Decimal('2'), Decimal('3'), Decimal('4'), Decimal('5'), Decimal('6')], "Should be [Decimal('2'), Decimal('3'), Decimal('4'), Decimal('5'), Decimal('6')]")
+        
+    def test_function4_firstArgumentsIsNotListType_shouldThrowErrorAndStopSystem(self):
+        with self.assertRaises((TypeError, SystemExit)): function4(("hello"), Decimal('1.22324224'))
+        with self.assertRaises((TypeError, SystemExit)): function4({'a':'b', 'c':'d'}, Decimal('2.12312312'))
+        with self.assertRaises((TypeError, SystemExit)): function4(Decimal('1.212131213213'), Decimal('0.1231241241'))
+        with self.assertRaises((TypeError, SystemExit)): function4(1021312312321, Decimal('0.1231241241'))
     
-    def test_function4_firstArgumentsIsNotListType_shouldReturnExpectedValues(self):
-        self.assertRaises(TypeError, function4(("hello"), Decimal('1.22324224')))
-        self.assertRaises(TypeError, function4({'a':'b', 'c':'d'}, Decimal('2.12312312')))
-        self.assertRaises(TypeError, function4(Decimal('1.212131213213'), Decimal('0.1231241241')))
-        self.assertRaises(TypeError, function4(1021312312321, Decimal('0.1231241241')))
-
+    def test_function4_secondArgumentsIsNotDecimalType_shouldThrowErrorAndStopSystem(self):
+        with self.assertRaises((TypeError, SystemExit)): function4([Decimal('1'),Decimal('0.25'),Decimal('1.25'),Decimal('10.0')], "hello")
+        with self.assertRaises((TypeError, SystemExit)): function4([Decimal('1.0005'),Decimal('2.0005'),Decimal('5.2532'),Decimal('0.0234')], {'a':'b', 'c':'d'})
+        with self.assertRaises((TypeError, SystemExit)): function4([Decimal('1'),Decimal('2'), Decimal('3'), Decimal('4'),Decimal('5')], True)
+        
+    #Tests for listToString() in main.py:
+    def test_listToString_allArgumentsAreValidInputs_shouldReturnExpectedValues(self):
+        self.assertEqual(listToString([Decimal('1.23123124'), Decimal('2.21312312312'), Decimal('0.21312312312'), Decimal('100.1232131221')]), "1.23123124, 2.21312312312, 0.21312312312, 100.1232131221", "Should be '1.23123124, 2.21312312312, 0.21312312312, 100.1232131221'")
+        self.assertEqual(listToString([1, 2, 4, 5, 6]), "1, 2, 4, 5, 6", "Should be '1, 2, 4, 5, 6'")
+        self.assertEqual(listToString(["INC21321", "INC123123", "INC123123", "INC321312"]), "INC21321, INC123123, INC123123, INC321312", "Should be 'INC21321, INC123123, INC123123, INC321312'")
+        self.assertEqual(listToString([Decimal('1.5'), Decimal('1.25'), Decimal('1.4'), Decimal('1.6')]), "1.5, 1.25, 1.4, 1.6", "Should be '1.5, 1.25, 1.4, 1.6'")
     
-    #Tests for floatArrayToString() in main.py:
-    def test_floatArrayToString(self):
-        self.assertEqual(floatArrayToString([Decimal(1.23123124), Decimal(2.21312312312), Decimal(0.21312312312), Decimal(100.1232131221)]), "1.23123124, 2.21312312312, 0.21312312312, 100.1232131221", "Should be 1.23123124, 2.21312312312, 0.21312312312, 100.1232131221")
-    
-    
-    print("Unit tests complete.")
-
-
-
+    def test_listToString_argumentsIsNotListType_shouldThrowErrorAndStopSystem(self):
+        with self.assertRaises((TypeError, SystemExit)): listToString("hello")
+        with self.assertRaises((TypeError, SystemExit)): listToString(True)
+        with self.assertRaises((TypeError, SystemExit)): listToString({'a':'b', 'c':'d'})
+        with self.assertRaises((TypeError, SystemExit)): listToString(500)
 
 if __name__ == '__main__':
     print("Performing unit tests...")
